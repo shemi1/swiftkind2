934d32bfbba545a2c8a27be325d2ca0e61c56be2
Earvin Gemenez <earvin.gemenez@gmail.com>
2019-05-07 11:49:32 +0800

    added maintenance

--- public/urls.py
+++ public/urls.py
@@ -4,5 +4,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='public/index.html'), name="index"),
     path('process/', TemplateView.as_view(template_name='public/process.html'), name="process"),
-    path('projects/', TemplateView.as_view(template_name='public/projects.html'), name="projects")
+    path('projects/', TemplateView.as_view(template_name='public/projects.html'), name="projects"),
+    path('maintenance/', TemplateView.as_view(template_name='public/maintenance.html'), name="maintenance"),
 ]
