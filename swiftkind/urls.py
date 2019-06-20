47cba940ba98f51532011917dd89fa351d194ba4
mcandidier <mannycandidier@gmail.com>
2019-06-20 14:46:13 +0800

    blog static pages

--- swiftkind/urls.py
+++ swiftkind/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('public.urls')),
+    path('blog/', TemplateView.as_view(template_name='blog/blog_index_page.html')),
+    path('blog/detail/', TemplateView.as_view(template_name='blog/blog.html')),
 ]
