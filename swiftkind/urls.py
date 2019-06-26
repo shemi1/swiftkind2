205ac3c38445bb7b5f66a601592d31eb808866e2
mcandidier <mannycandidier@gmail.com>
2019-06-26 13:57:05 +0800

    add main image to a blog

--- swiftkind/urls.py
+++ swiftkind/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('public.urls')),
     path('', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
